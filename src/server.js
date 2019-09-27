e457f23385ef8c20570b1ec9795a8b2f0683f296
Steven Bull <stevenwbulljr@gmail.com>
2019-09-27 15:23:32 -0500

    remove console logs

--- src/server.js
+++ src/server.js
@@ -1,7 +1,4 @@
 const app = require('./app');
 const { PORT } = require('./config');
 
-app.listen(PORT, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Server listening at http://localhost:${PORT}`);
-});
+app.listen(PORT);
