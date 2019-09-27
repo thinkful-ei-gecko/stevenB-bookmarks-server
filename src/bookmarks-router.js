4175955dde85c1c0cb8d2f284ea70f286f1a1ed8
Steven Bull <stevenwbulljr@gmail.com>
2019-09-27 11:06:22 -0500

    complete dataStore.js integration

--- src/bookmarks-router.js
+++ src/bookmarks-router.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
+const [ ...bookmark ] = require('./dataStore');
 const logger = require('./logger');
 
 const bookmarksRouter = express.Router();
 
 bookmarksRouter.get('/', ( req, res ) => {
   logger.info('Bookmarks data accessed');
-  res.send('Bookmarks are working!');
+  res.send(bookmark);
 });
 
 module.exports = bookmarksRouter;
