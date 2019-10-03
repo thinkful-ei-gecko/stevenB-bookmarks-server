2fa09d15b164aad6289ff0dc3007b37ec1bb85f5
Steven Bull <stevenwbulljr@gmail.com>
2019-10-03 11:23:35 -0500

    finish seed file

--- migrations/001.undo.create_bookmarks.sql
+++ migrations/001.undo.create_bookmarks.sql
@@ -1,1 +1,2 @@
 DROP TABLE IF EXISTS bookmark_items;
+DROP TYPE rating_num;
---
+++ seeds/seed.bookmark_items.sql
@@ -0,0 +1,9 @@
+INSERT INTO bookmark_items (title, url, description, rating) 
+VALUES
+  ('Google', 'https://www.Google.com', 'Super sweeet', '5'),
+  ('Facebook', 'https://www.Facebook.com', 'Super sweeet', '1'),
+  ('Yahoo', 'https://www.Yahoo.com', 'Super sweeet', '2'),
+  ('Spotify', 'https://www.Spotify.com', 'Super sweeet', '5'),
+  ('MSN', 'https://www.MSN.com', 'Super sweeet', '3'),
+  ('Reddit', 'https://www.Reddit.com', 'Super sweeet', '4')
+;
